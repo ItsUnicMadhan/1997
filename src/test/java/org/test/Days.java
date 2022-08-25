067b5c56664dc09e4c9e121d8b0e2402f1e2a8b1
ItsUnicMadhan <jm28091997@gmail.com>
2022-08-25 19:22:45 +0530

    1st Time

---
+++ src/test/java/org/test/Days.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Days {
+
+	private void Sunday() {
+		System.out.println("Day1");
+		
+	}
+	private void Monday() {
+		System.out.println("Day2");
+
+	}
+	private void Tuesday() {
+		System.out.println("Day3");
+
+	}
+}
