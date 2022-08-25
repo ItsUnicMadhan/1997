7e76df18310a21fcb943c2b66e32ef944abaf706
ItsUnicMadhan <jm28091997@gmail.com>
2022-08-25 19:55:04 +0530

    3rd Time

--- src/test/java/org/test/Days.java
+++ src/test/java/org/test/Days.java
@@ -14,4 +14,16 @@ public class Days {
 		System.out.println("Day3");
 
 	}
+	private void Week() {
+		System.out.println("52 Weeks");
+		
+	}
+	private void Month() {
+		System.out.println("12 Months");
+
+	}
+	private void Year() {
+		System.out.println("1 Year");
+
+	}
 }
